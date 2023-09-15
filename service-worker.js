c729696a731fbf77b22c7abe18ff5dc01ec3c897
Dhiraj Kumar Varma Dommaraju <ddommaraju@csuchico.edu>
2023-09-15 15:16:12 -0700

    Portfolio app to gpages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df95ab80efebbf70c7a001b90ca9f8e1.js"
+  "/portfolio/precache-manifest.7cf3b6524a73f10c932f1ba77c3ec6c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
