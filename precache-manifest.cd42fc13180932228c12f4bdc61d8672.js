8444f35f6ec747e2b338d7acc8b2a98895e80957
Dhiraj Kumar Varma Dommaraju <ddommaraju@csuchico.edu>
2023-09-15 15:29:05 -0700

    Portfolio app to gpages

---
+++ precache-manifest.cd42fc13180932228c12f4bdc61d8672.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9869c464ae59ffee91cc743547d55b2",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "90096dd1d830d1a9d658",
+    "url": "/Portfolio/static/css/main.33c0eb41.chunk.css"
+  },
+  {
+    "revision": "64770be9759ac2eb893d",
+    "url": "/Portfolio/static/js/2.75bd9dfa.chunk.js"
+  },
+  {
+    "revision": "90096dd1d830d1a9d658",
+    "url": "/Portfolio/static/js/main.0f787701.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "77723fd630322da26f3279181b35ff41",
+    "url": "/Portfolio/static/media/dhi2.77723fd6.jpg"
+  },
+  {
+    "revision": "ca065b6514ad03fd5cf589eee5baeaee",
+    "url": "/Portfolio/static/media/dhiraj-dommaraju-resume.ca065b65.pdf"
+  }
+]);
